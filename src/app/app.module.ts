38b8cd3a883e6dbaaa8d647d1ef56e335eca8db9
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2020-07-17 17:48:17 -0300

    Segunda Etapa - Lidando com componentes concluída

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/courseList.component'
 import { StarComponent } from './star/star.component'
+import { ReplacePipe } from './pipe/replace.pipe'
+import { NavBarComponent } from './navbar/navbar.component'
+import { NotFoundComponent } from './notFound/notFound.component'
+import { CourseInfoComponent } from './courses/courseInfo.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    NotFoundComponent,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: '**', component: NotFoundComponent 
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/courses/courseInfo.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router';
+import { Course } from './course'
+import { CourseService } from './course.service';
+
+@Component({
+    templateUrl: './courseInfo.component.html'
+})
+
+export class CourseInfoComponent implements OnInit{
+
+    course: Course;
+
+    constructor(private ActivatedRoute: ActivatedRoute, private courseService: CourseService){}
+
+    ngOnInit(): void{
+
+        this.course = this.courseService.retrieveById(+this.ActivatedRoute.snapshot.paramMap.get('id'))
+
+    }
+
+    save():void {
+        this.courseService.save(this.course)
+    }
+
+}
